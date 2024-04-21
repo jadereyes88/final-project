f15dee9b960d24e653e157418645a004002d21e1
jadereyes88 <122693715+jadereyes88@users.noreply.github.com>
2024-04-20 21:35:56 -0400

    Update seeder_file.sql

--- seeder_file.sql
+++ seeder_file.sql
@@ -1,9 +1,29 @@
---create table
+-- create contacts table
 CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     subject VARCHAR(255),
-    message TEXT,
+    message VARCHAR(1000)  
 );
+
+-- insert dummy data into contacts table
+INSERT INTO contacts (name, email, subject, message) VALUES 
+('Brad Pitt', 'brad.pitt@example.com', 'Inquiry', 'Hello, I have a question about your services.'),
+('Jessica Alba', 'jessica.alba@example.com', 'Feedback', 'Great service! Thank you.'),
+('Margot Robbie', 'margot.robbie@example.com', 'Support', 'Can we get some help?');
+
+--- create projects table 
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    project_name VARCHAR(255) NOT NULL,
+    camera_type VARCHAR(1000),  
+    image_url VARCHAR(100) 
+);
+
+-- insert dummy data into projects table
+INSERT INTO projects project_name, camera_type, image_url) VALUES 
+('Roses', 'Canon EOS 541', 'http://random_url.com'),
+('Running', 'Canon EOS 541', 'http://random_url.com'),
+('Batman', 'Canon EOS 541', 'http://random_url.com');;
 
